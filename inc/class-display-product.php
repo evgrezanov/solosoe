970425ca1a42ed4c343029d371a83968d8d8e52d
Evgeniy Rezanov <evgrezanov@gmail.com>
2020-02-02 12:44:10 +0700

    del comments

--- inc/class-display-product.php
+++ inc/class-display-product.php
@@ -180,46 +180,11 @@ class SOLOSOE_DISPLAY_PRODUCT {
                             <dt>Delivery</dt>
                             <dd>Russia, USA, and Europe</dd>
                         </dl>  <!-- item-property-hor .// -->
-                        <hr>
-                            <div class="row">
-                                <div class="col-sm-5">
-                                    <dl class="param param-inline">
-                                    <dt>Quantity: </dt>
-                                    <dd>
-                                        <select class="form-control form-control-sm" style="width:70px;">
-                                            <option> 1 </option>
-                                            <option> 2 </option>
-                                            <option> 3 </option>
-                                        </select>
-                                    </dd>
-                                    </dl>  <!-- item-property .// -->
-                                </div> <!-- col.// -->
-                                <div class="col-sm-7">
-                                    <dl class="param param-inline">
-                                        <dt>Size: </dt>
-                                        <dd>
-                                            <label class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-                                            <span class="form-check-label">SM</span>
-                                            </label>
-                                            <label class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-                                            <span class="form-check-label">MD</span>
-                                            </label>
-                                            <label class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-                                            <span class="form-check-label">XXL</span>
-                                            </label>
-                                        </dd>
-                                    </dl>  <!-- item-property .// -->
-                                </div> <!-- col.// -->
-                            </div> <!-- row.// -->
-                            <hr>
-                            <a href="#" class="btn btn-lg btn-primary text-uppercase"> Buy now </a>
-                            <a href="#" class="btn btn-lg btn-outline-primary text-uppercase"> <i class="fas fa-shopping-cart"></i> Add to cart </a>
+                        <a href="#" class="btn btn-lg btn-primary text-uppercase"> Buy now </a>
+                        <a href="#" class="btn btn-lg btn-outline-primary text-uppercase"> <i class="fas fa-shopping-cart"></i> Add to cart </a>
                     </article> <!-- card-body.// -->
-                                </aside> <!-- col.// -->
-                            </div> <!-- row.// -->
+                </aside> <!-- col.// -->
+            </div> <!-- row.// -->
         </div> <!-- card.// -->
         <?php 
         return ob_get_clean();
@@ -234,7 +199,7 @@ class SOLOSOE_DISPLAY_PRODUCT {
     public static function get_product_price($product_id, $price_url) {
         
         $price = json_decode(file_get_contents($price_url));
-        var_dump($price);
+        //var_dump($price);
         return $price;
     }
    
@@ -242,9 +207,9 @@ class SOLOSOE_DISPLAY_PRODUCT {
     *  Get product info by API
     */
     public static function get_product_info($product_id, $info_url) {
-        var_dump($info_url);
+        //var_dump($info_url);
         $data = json_decode(file_get_contents($info_url));
-        var_dump($data);
+        //var_dump($data);
         return $data;
     }
 
