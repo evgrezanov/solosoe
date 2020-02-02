a5336127854a58c12635c5130f5a2252e62c2278
Evgeniy Rezanov <evgrezanov@gmail.com>
2020-02-02 14:09:00 +0700

    clear

--- inc/class-display-product.php
+++ inc/class-display-product.php
@@ -48,11 +48,8 @@ class SOLOSOE_DISPLAY_PRODUCT {
 
             // get product data
             if ( !empty($prd_price_url) && !empty($prd_info_url) ):
-                $product_info = self::get_product_info($product_id, $prd_info_url);
-                $product_price = self::get_product_price($product_id, $prd_price_url);
-                
-                //echo self::display_product_details($product_id, $product_info, $product_price);
-                //echo self::display_product_details2();
+                $product_info = json_decode(file_get_contents($prd_info_url));
+                $product_price = json_decode(file_get_contents($prd_price_url));
                 echo self::display_product_card($product_id, $product_info, $product_price);
             endif;
 
@@ -60,212 +57,96 @@ class SOLOSOE_DISPLAY_PRODUCT {
     }
 
    /**
-    *   Display product detail with id < 6
-    *   http://34.243.79.103:8000/services/product/
-    *   http://34.243.79.103:8000/services/optimal-price/
+    * Help function for diusplay avalible shops
     */
-    public static function display_product_details($product_id, $product_info, $product_price){
-    $master_details = $product_info->master_details;
-    ob_start();
-    ?>
-    <div class="row">
-        <div class="card">
-            <div class="row no-gutters">
-                <div class="col-md-4">
-                <img src="<?php echo $product_info->images[1]; ?>" class="card-img" alt="<?php echo $product_info->product_name; ?>">
-            </div>
-            <div class="col-md-8">
-                
-                <div class="card-header">
-                    <h5 class="card-title"><?php echo $product_info->product_name; ?></h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $product_info->ean; ?></h6>
-                    <small class="text-muted"><?php echo $product_info->cn_dot_7; ?></small>
-                    <div class="text-warning text-center mt-3">
-                        <h4>Precio Competitevo</h4>
-                    </div>
-                    <div class="text-warning text-center mt-2">
-                        <h1><?php echo $product_price->price; ?></h1>
-                    </div>
-                    
-                </div>
-                
-                <div class="card-body">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Rango de precios: <?php echo $product_info->min_sale_price.' - '.$product_info->max_sale_price; ?></li>
-                        <li class="list-group-item">№ de Comercios: <?php echo $product_info->mst_prd_id; ?></li>
-                        <li class="list-group-item">no_of_shops: <?php echo $product_info->no_of_shops; ?></li>
-                    </ul>
-                    <p class="card-text">
-                        <?php echo $product_info->product_description; ?>
-                    </p>
-                    <?php 
-                        if (isset($master_details)):
-                            do_action('display_shops', $master_details); 
-                        endif;    
-                    ?>
-                </div>
-                
-                <div class="card-footer">
-                    <p class="card-text">
-                        <?php echo $product_info->product_description; ?>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php 
-    return ob_get_clean();
-    }
-    
-
     public static function display_shops($master_details){ ?>
-    <div class='list-bottom'>
-    <?php foreach ($master_details as $details): ?>
+        <div class='list-bottom'>
+        <?php foreach ($master_details as $details): ?>
             <div class='list-bottom-section'>
                 <span><?php echo $details->marketplace;?></span>
                 <span class="badge badge-success"><?php echo $details->no_of_shops;?></span>
                 <span><?php echo $details->min_price . ' - ' . $details->max_price;?></span>
             </div>
-    <?php endforeach; ?> 
+        <?php endforeach; ?> 
         </div>
       <?php
     }
 
+
+   /**
+    *   Display product detail with id < 6
+    *   http://34.243.79.103:8000/services/product/{{144615}}/
+    *   http://34.243.79.103:8000/services/optimal-price/{144615}/
+    *
+    *   Display product detail with id > 6    
+    *   https://cima.aemps.es/cima/rest/psuministro/{{912485}}
+    *   https://cima.aemps.es/cima/rest/medicamento?cn={{912485}}
+    */
     public static function display_product_card($product_id, $product_info, $product_price){
-    $master_details = $product_info->master_details;
-    ob_start();
-    ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col-auto d-none d-lg-block">
-                        <img src="<?php echo $product_info->images[0]; ?>" class="card-img" alt="<?php echo $product_info->product_name; ?>">
-                    </div>
-                    <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-primary"><?php echo '#'.$product_id; ?></strong>
-                        
-                        <h3 class="mb-0"><?php echo $product_info->product_name; ?></h3>
-                        
-                        <h4>Precio Competitevo <span class="badge badge-warning"><?php echo $product_price->price; ?> &#8364;</span></h4>
-                        
-                        <div class="mb-1 text-muted">
-                            <?php echo $product_info->ean; ?>
-                        </div>
-                        
-                        <ul class="list-group list-group-flush">
-                            <?php if (isset($product_info->min_sale_price) && isset($product_info->max_sale_price)): ?>
-                                <li class="list-group-item">Rango de precios: <?php echo $product_info->min_sale_price.' - '.$product_info->max_sale_price; ?> &#8364; </li>
-                            <?php endif; ?>
-
-                            <?php if (isset($product_info->mst_prd_id)): ?>
-                                <li class="list-group-item">№ de Comercios: <?php echo $product_info->mst_prd_id; ?></li>
-                            <?php endif; ?> 
-
-                            <?php if (isset($product_info->no_of_shops)): ?>
-                                <li class="list-group-item">no_of_shops: <?php echo $product_info->no_of_shops; ?></li>
-                            <?php endif; ?>    
-                        </ul>
-                        
-                        <div class="card-text">
-                            <?php if (!empty($master_details)): self::display_shops($master_details); endif; ?>
-                        </div>
-                        
-                            <!--<a href="#" class="stretched-link">Continue reading</a>-->
-                    </div>
-                    <div class="card-footer">
-                        <p class="card-text mb-auto">
+        $master_details = $product_info->master_details;
+        ob_start();
+        ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div class="card-header">
                             <?php echo $product_info->product_description; ?>
-                        </p>
+                        </div>
+                        
+                        <div class="col-auto d-none d-lg-block">
+                            <!--img-->
+                            <img src="<?php echo $product_info->images[0]; ?>" class="card-img" alt="<?php echo $product_info->product_name; ?>">
+                        </div>
+                        
+                        <div class="col p-4 d-flex flex-column position-static">
+                            <!--id-->
+                            <strong class="d-inline-block mb-2 text-primary"><?php echo '#'.$product_id; ?></strong>
+                            
+                            <!--product name-->
+                            <h3 class="mb-0"><?php echo $product_info->product_name; ?></h3>
+                            
+                            <!--price-->
+                            <h4>Precio Competitevo <span class="badge badge-warning"><?php echo $product_price->price; ?> &#8364;</span></h4>
+                            
+                            <!--product properties-->
+                            <ul class="list-group list-group-flush">
+                                <?php if (isset($product_info->min_sale_price) && isset($product_info->max_sale_price)): ?>
+                                    <li class="list-group-item">Rango de precios: <?php echo $product_info->min_sale_price.' - '.$product_info->max_sale_price; ?> &#8364; </li>
+                                <?php endif; ?>
+
+                                <?php if (isset($product_info->mst_prd_id)): ?>
+                                    <li class="list-group-item">№ de Comercios: <?php echo $product_info->mst_prd_id; ?></li>
+                                <?php endif; ?> 
+
+                                <?php if (isset($product_info->no_of_shops)): ?>
+                                    <li class="list-group-item">no_of_shops: <?php echo $product_info->no_of_shops; ?></li>
+                                <?php endif; ?>
+
+                                <?php if (isset($product_info->ean)): ?>
+                                    <li class="list-group-item">ean: <?php echo $product_info->ean; ?></li>
+                                <?php endif; ?>
+                            </ul>
+                            
+                            <!--shops-->
+                            <div class="card-text">
+                                <?php if (!empty($master_details)): self::display_shops($master_details); endif; ?>
+                            </div>
+                            
+                                <!--<a href="#" class="stretched-link">Continue reading</a>-->
+                        </div>
+                        
+                        <div class="card-footer">
+                            <p class="card-text mb-auto">
+                                <?php echo $product_info->product_description; ?>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <?php
+        <?php
         return ob_get_clean();
-    }
-   /**
-    *   Display product detail with id > 6
-    *   https://cima.aemps.es/cima/rest/medicamento?cn={{product_id}}
-    *   https://cima.aemps.es/cima/rest/psuministro/{{product_id}}
-    */
-    public static function display_product_details2(){
-        ob_start();
-        ?>
-        <div class="card">
-	        <div class="row">
-		        <aside class="col-sm-5 border-right">
-                    <article class="gallery-wrap"> 
-                        <div class="img-big-wrap">
-                            <div> <a href="#"><img src="https://s9.postimg.org/tupxkvfj3/image.jpg"></a></div>
-                        </div> <!-- slider-product.// -->
-                        <div class="img-small-wrap">
-                            <div class="item-gallery"> <img src="https://s9.postimg.org/tupxkvfj3/image.jpg"> </div>
-                            <div class="item-gallery"> <img src="https://s9.postimg.org/tupxkvfj3/image.jpg"> </div>
-                            <div class="item-gallery"> <img src="https://s9.postimg.org/tupxkvfj3/image.jpg"> </div>
-                            <div class="item-gallery"> <img src="https://s9.postimg.org/tupxkvfj3/image.jpg"> </div>
-                        </div> <!-- slider-nav.// -->
-                    </article> <!-- gallery-wrap .end// -->
-		        </aside>
-		        <aside class="col-sm-7">
-                    <article class="card-body p-5">
-	                    <h3 class="title mb-3">Original Version of Some product name</h3>
-                        <p class="price-detail-wrap"> 
-	                        <span class="price h3 text-warning"> 
-		                        <span class="currency">US $</span><span class="num">1299</span>
-	                        </span> 
-	                        <span>/per kg</span> 
-                        </p> <!-- price-detail-wrap .// -->
-                        <dl class="item-property">
-                            <dt>Description</dt>
-                            <dd><p>Here goes description consectetur adipisicing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco </p></dd>
-                        </dl>
-                        <dl class="param param-feature">
-                            <dt>Model#</dt>
-                            <dd>12345611</dd>
-                        </dl>  <!-- item-property-hor .// -->
-                        <dl class="param param-feature">
-                            <dt>Color</dt>
-                            <dd>Black and white</dd>
-                        </dl>  <!-- item-property-hor .// -->
-                        <dl class="param param-feature">
-                            <dt>Delivery</dt>
-                            <dd>Russia, USA, and Europe</dd>
-                        </dl>  <!-- item-property-hor .// -->
-                        <a href="#" class="btn btn-lg btn-primary text-uppercase"> Buy now </a>
-                        <a href="#" class="btn btn-lg btn-outline-primary text-uppercase"> <i class="fas fa-shopping-cart"></i> Add to cart </a>
-                    </article> <!-- card-body.// -->
-                </aside> <!-- col.// -->
-            </div> <!-- row.// -->
-        </div> <!-- card.// -->
-        <?php 
-        return ob_get_clean();
-        }
-    
-   
-    
-   
-   /**
-    *  Get product  price by API
-    */
-    public static function get_product_price($product_id, $price_url) {
-        
-        $price = json_decode(file_get_contents($price_url));
-        //var_dump($price);
-        return $price;
-    }
-   
-   /**
-    *  Get product info by API
-    */
-    public static function get_product_info($product_id, $info_url) {
-        //var_dump($info_url);
-        $data = json_decode(file_get_contents($info_url));
-        //var_dump($data);
-        return $data;
     }
 
 
