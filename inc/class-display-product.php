a1af7921ebe2a26cbb61955d8c581190cda2faeb
Evgeniy Rezanov <evgrezanov@gmail.com>
2020-02-07 14:14:51 +0700

    display resultados

--- inc/class-display-product.php
+++ inc/class-display-product.php
@@ -36,43 +36,54 @@ class SOLOSOE_DISPLAY_PRODUCT {
             $product_id = $_REQUEST['prd_id'];
         
             // check id and chose corret link
-            $id_start = (int)substr($product_id, 0, 1);
+            //$id_start = (int)substr($product_id, 0, 1);
             
             //to do >= or > ????????????
-            if ($id_start >= 6):
+            /*if ($id_start >= 6):
                 $prd_info_url = 'https://cima.aemps.es/cima/rest/medicamento?cn='.$product_id;
                 $prd_price_url = 'https://cima.aemps.es/cima/rest/psuministro/'.$product_id;
             else:
                 $prd_info_url = 'http://34.243.79.103:8000/services/product/'.$product_id.'/?format=json';
                 $prd_price_url = 'http://34.243.79.103:8000/services/optimal-price/'.$product_id.'/?format=json';
-            endif;
+            endif;*/
+            
+            $prd_info_url = 'http://34.243.79.103:8000/services/product/'.$product_id.'/?format=json';
+            $prd_price_url = 'http://34.243.79.103:8000/services/optimal-price/'.$product_id.'/?format=json';
 
             // get product data
             if ( !empty($prd_price_url) && !empty($prd_info_url) ):
                 
                 $info = file_get_contents($prd_info_url);
-                if ($info['reponse_code'] != 200):
-                    echo self::display_no_result($http_response_header);
-                    die();
-                endif;
-
                 
                 $price = file_get_contents($prd_price_url);
-                if ($price['reponse_code'] != 200):
-                    echo self::display_no_result($http_response_header);
-                    die();
-                endif;
 
                 $product_info = json_decode($info);
                 $product_price = json_decode($price);
                 
                 echo self::display_product_card($product_id, $product_info, $product_price);
+                
+                // check - have cima info?
+                if (isset($product_info->cn_dot_7)):
+                    $id_start = (int)substr($product_info->cn_dot_7, 0, 1);
+                    // if id_start > 6 cima have info
+                    if ($id_start >= 6):
+                        
+                        $cn_dot_7 = explode(".", $product_info->cn_dot_7);
+                        $cima_id = $cn_dot_7[0];
+                        
+                        $cima_medicamento = json_decode(file_get_contents('https://cima.aemps.es/cima/rest/medicamento?cn='.$cima_id));
+                        $cima_psuministro = json_decode(file_get_contents('https://cima.aemps.es/cima/rest/psuministro/'.$cima_id));
+                        echo self::display_cima_medicamento($cima_medicamento);
+                        echo self::display_cima_psuministro($cima_psuministro);
+                    endif;
+                endif;
+
             endif;
 
         endif;    
     }
    
-    /**
+   /**
     * display no-results message
     */    
     public static function display_no_result($http_response_header){
@@ -85,7 +96,7 @@ class SOLOSOE_DISPLAY_PRODUCT {
     }
 
    /**
-    * Help function for diusplay avalible shops
+    * Help function for display avalible shops
     */
     public static function display_shops($master_details){ ?>
         <div class='list-bottom'>
@@ -105,10 +116,6 @@ class SOLOSOE_DISPLAY_PRODUCT {
     *   Display product detail with id < 6
     *   http://34.243.79.103:8000/services/product/{{144615}}/
     *   http://34.243.79.103:8000/services/optimal-price/{144615}/
-    *
-    *   Display product detail with id > 6    
-    *   https://cima.aemps.es/cima/rest/psuministro/{{912485}}
-    *   https://cima.aemps.es/cima/rest/medicamento?cn={{912485}}
     */
     public static function display_product_card($product_id, $product_info, $product_price){
         $master_details = $product_info->master_details;
@@ -136,6 +143,15 @@ class SOLOSOE_DISPLAY_PRODUCT {
                             
                             <!--product properties-->
                             <ul class="list-group list-group-flush">
+                                
+                                <?php if (isset($product_info->ean)): ?>
+                                    <li class="list-group-item">ean: <?php echo $product_info->ean; ?></li>
+                                <?php endif; ?>
+
+                                <?php if (isset($product_info->cn_dot_7)): ?>
+                                    <li class="list-group-item">cn_dot_7: <?php echo $product_info->cn_dot_7; ?></li>
+                                <?php endif; ?>
+
                                 <?php if (isset($product_info->min_sale_price) && isset($product_info->max_sale_price)): ?>
                                     <li class="list-group-item">Rango de precios: <?php echo $product_info->min_sale_price.' - '.$product_info->max_sale_price; ?> &#8364; </li>
                                 <?php endif; ?>
@@ -148,9 +164,7 @@ class SOLOSOE_DISPLAY_PRODUCT {
                                     <li class="list-group-item">no_of_shops: <?php echo $product_info->no_of_shops; ?></li>
                                 <?php endif; ?>
 
-                                <?php if (isset($product_info->ean)): ?>
-                                    <li class="list-group-item">ean: <?php echo $product_info->ean; ?></li>
-                                <?php endif; ?>
+                                
                             </ul>
                             
                             <!--shops-->
@@ -190,7 +204,63 @@ class SOLOSOE_DISPLAY_PRODUCT {
         return $head;
     }
 
+    public static function display_cima_medicamento($medicamento){
+    // for example
+    // $cima_info_url = https://cima.aemps.es/cima/rest/medicamento?cn=912485
+    ob_start();
+    ?>
+        <ul class="cima-medicamento list-group">
+            <li class="list-group-item"><?php echo 'nregistro: '.$medicamento->nregistro; ?></li>
+            <li class="list-group-item"><?php echo 'pactivos: '.$medicamento->pactivos; ?></li>
+            <li class="list-group-item"><?php echo 'labtitular: '.$medicamento->labtitular; ?></li>
+            <li class="list-group-item"><?php echo 'cpresc: '.$medicamento->cpresc; ?></li>
+            <li class="list-group-item"><?php echo 'dosis: '.$medicamento->dosis; ?></li>
+        </ul>
+    <?php
+    return ob_get_clean();
+        /*
+        $cima_info->nosustituible
+        $docs = $cima_info->docs.tipo.url.fecha
+        $docs->tipo
+        $docs->url
+        $docs->fecha
+        $cima_info->atcs.codigo.nombre.nivel
+        $atcs->codigo
+        $atcs->nombre
+        $atcs->nivel
+        $cima_info->principiosActivos.id.codigo.nombre.cantidad.unidad.orden
+        $principiosActivos->id
+        $principiosActivos->codigo
+        $principiosActivos->nombre
+        $principiosActivos->cantidad
+        $principiosActivos->unidad
+        $principiosActivos->orden
+        $cima_info->excipientes.id.nombre.cantidad.unidad.orden
+        $cima_info->viasAdministracion.id.nombre
+        $cima_info->presentaciones.cn.nombre.comerc.psum
+        $cima_info->presentaciones.estado.aut
+        $cima_info->formaFarmaceutica.id.nombre
+        $cima_info->formaFarmaceuticaSimplificada.id.nombre
+        $cima_info->vtm.id.nombre
+        $cima_info->dosis
+*/
+    }
 
+    public static function display_cima_psuministro($psuministro){
+    // for example
+    // $cima_price_url = https://cima.aemps.es/cima/rest/psuministro/912485
+    $resultados = $psuministro->resultados;
+    $resultados = $resultados[0];
+    foreach ($resultados as $key=>$value):
+    ob_start();
+        ?>
+        <ul class="cima-psuministro list-group">
+            <li class="list-group-item"><?php echo $key.': '.$value; ?></li>
+        </ul>
+    <?php
+    endforeach;
+    return ob_get_clean();
+    }
 
 }
 
